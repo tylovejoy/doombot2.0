4844e9dca19b94d1ae0ef64edcb52b170c09f5cb
Ty Lovejoy <48333668+tylovejoy@users.noreply.github.com>
2021-12-23 09:24:02 -0600

    abtract more of tournament

--- database/tournament.py
+++ database/tournament.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from datetime import datetime
 from logging import getLogger
-from typing import Optional, List, Literal
+from typing import Generator, Optional, List, Literal, Tuple
 
 from beanie import Document
 from pydantic import BaseModel
@@ -47,10 +47,10 @@ class TournamentMissions(BaseModel):
 class TournamentMissionsCategories(BaseModel):
     """Base model for tournament mission categories."""
 
-    easy: Optional[TournamentMissions] = TournamentMissions()
-    medium: Optional[TournamentMissions] = TournamentMissions()
-    hard: Optional[TournamentMissions] = TournamentMissions()
-    expert: Optional[TournamentMissions] = TournamentMissions()
+    easy: TournamentMissions = TournamentMissions()
+    medium: TournamentMissions = TournamentMissions()
+    hard: TournamentMissions = TournamentMissions()
+    expert: TournamentMissions = TournamentMissions()
 
 
 class TournamentData(BaseModel):
@@ -67,8 +67,6 @@ class Tournament(Document):
     tournament_id: int
     name: str
     active: bool
-    bracket: bool
-    bracket_category: Optional[str]
     schedule_start: datetime
     schedule_end: datetime
 
@@ -77,38 +75,43 @@ class Tournament(Document):
     hc: Optional[TournamentData]
     bo: Optional[TournamentData]
 
-    general_mission: Optional[TournamentMissions] = TournamentMissions()
+    general_mission: TournamentMissions = TournamentMissions()
 
     @classmethod
     async def find_latest(cls) -> Tournament:
         return (await cls.find().sort("-tournament_id").limit(1).to_list())[0]
 
+    def get_categories(self) -> Generator[str]:
+        for cat in ["ta", "mc", "hc", "bo"]:
+            if getattr(self, cat, None):
+                yield cat
+
     def get_map_str(self, category: CategoryLiteral) -> str:
-        category = getattr(self, category)
+        category = getattr(self, category, None)
         return f"{category.map_data.code} - {category.map_data.level} ({category.map_data.map_name}) by {category.map_data.creator}\n"
 
     def get_all_map_str(self) -> str:
         map_string = ""
-        for category in ["ta", "mc", "hc", "bo"]:
+        for category in self.get_categories():
             map_string += self.get_map_str(category)
         return map_string
 
     def get_records(self, category: CategoryLiteral) -> List[TournamentRecords]:
-        category = getattr(self, category)
+        category = getattr(self, category, None)
         return category.records
 
     def get_difficulty_missions(self, difficulty: DifficultyLiteral) -> str:
         missions = ""
-        for cat in ["ta", "mc", "hc", "bo"]:
-            category = getattr(getattr(self, cat).missions, difficulty)
+        for cat in self.get_categories():
+            category = getattr(getattr(self, cat, None).missions, difficulty, None)
             missions += f"{category.type} - {category.target}\n"
         return missions
 
     def get_category_missions(self, category: CategoryLiteral) -> str:
-        obj = getattr(self, category)
+        obj = getattr(self, category, None)
         missions = ""
         for difficulty in ["easy", "medium", "hard", "expert"]:
-            curr = getattr(obj.missions, difficulty)
+            curr = getattr(obj.missions, difficulty, None)
             missions += format_missions(curr, difficulty)
 
         return missions
@@ -117,8 +120,9 @@ class Tournament(Document):
         missions = ""
         map_ = {"ta": "Time Attack", "mc": "Mildcore", "hc": "Hardcore", "bo": "Bonus"}
         for category in map_.keys():
-            missions += f"**{map_[category]}:**\n"
-            missions += self.get_category_missions(category)
+            if category in self.get_categories()
+                missions += f"**{map_[category]}:**\n"
+                missions += self.get_category_missions(category)
         return missions
 
     def get_general_mission(self) -> str:
