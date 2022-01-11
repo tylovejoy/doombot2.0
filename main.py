5895ddb6e676f4c9f8f13074329b998206ea1489
Ty Lovejoy <48333668+tylovejoy@users.noreply.github.com>
2022-01-11 10:13:57 -0600

    add logging to startup

--- main.py
+++ main.py
@@ -21,20 +21,22 @@ bot = DoomBot()
 
 def load_all_extensions():
     """Load all slashes."""
-    logger.info("Loading slash...")
+    logger.info("Loading slash commands...")
     parents.setup(bot)
     maps.setup(bot)
     records.setup(bot)
     tournament.setup(bot)
     exp.setup(bot)
     tags.setup(bot)
-    logger.info("Slash loaded.")
+    logger.info("Finished. Slash commands loaded.")
 
 
 @bot.event
 async def setup():
     """Upload slash commands to discord."""
+    logger.info("Uploading guild slash commands to Discord...")
     await bot.upload_guild_application_commands()
+    logger.info("Finished. Guild slash commands uploaded to Discord...")
 
 
 TOKEN = environ["TOKEN"]
