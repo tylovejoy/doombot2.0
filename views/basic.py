b8378981127730ee767f378631b17b924f1f9b51
Ty Lovejoy <Ty@fromskytocenter.com>
2022-02-12 10:18:12 -0600

    baseview testing

--- views/basic.py
+++ views/basic.py
@@ -1,4 +1,25 @@
+import logging
+
 import discord
+
+
+logger = logging.getLogger()
+
+
+class BaseView(discord.ui.View):
+    def __init__(self, author: discord.Member):
+        super().__init__(timeout=None)
+        self.author = author
+
+    async def interaction_check(self, item, interaction: discord.Interaction):
+        if self.author == interaction.user:
+            return True
+        else:
+            await interaction.response.send_message(
+                "You can't interact with these buttons as you weren't the one using the command.",
+                ephemeral=True,
+            )
+            return False
 
 
 class ConfirmButton(discord.ui.Button):
