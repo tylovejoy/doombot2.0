f7753a83aeb4b440ef214a439283dd4564f2789e
Ty Lovejoy <48333668+tylovejoy@users.noreply.github.com>
2022-03-03 14:50:44 -0600

    implement some store stuff

--- slash/store.py
+++ slash/store.py
@@ -1,22 +1,52 @@
 from typing import Literal
 
 import discord
+from utils.embed import create_embed
 
 from utils.constants import GUILD_ID
+from views.store import MainStoreView
+
+
+def setup(bot):
+    bot.application_command(OpenStore)
 
 
 class OpenStore(discord.SlashCommand, guilds=[GUILD_ID], name="store"):
     """View the XP store."""
 
-    category: Literal["Emotes", "Roles"] = discord.Option(
-        "Which category in the store would you like to browse?"
-    )
-
     async def callback(self) -> None:
-        if self.category == "Emotes":
-            # Shop Emotes View
-            return
+        await self.defer(ephemeral=True)
+        main_menu = create_embed(
+            "Doombot General Store",
+            "Welcome! Select any of the buttons below to browse the store.",
+            self.interaction.user,
+        )
+        emote_store = create_embed(
+            "Emotes",
+            "Here you can buy emote/sticker slots.",
+            self.interaction.user,
+        )
+        role_store = create_embed(
+            "Roles",
+            "Here you can buy custom roles.",
+            self.interaction.user,
+        )
+        view = MainStoreView(self.interaction, emotes=emote_store, roles=role_store, main=main_menu)
+        await view.start()
 
-        if self.category == "Roles":
-            # Shop Roles View
+        await view.wait()
+
+        if not view.confirm.value:
             return
+        
+        await self.interaction.edit_original_message(content="Check your DMs to continue the transaction.", embed=None, view=None)
+
+        if view.emote_dropdown.values[0] == "Standard Emoji":
+            ...
+        await self.interaction.user.send("Respond with the emoji/sticker that you want to add.")
+        def check(message: discord.Message):
+            return message.guild == None and message.author.id == self.interaction.user.id
+        await self.client.wait_for("message", check=check, timeout=60)
+        await self.interaction.user.send("Respond with the name that you want to give the emoji/sticker.")
+
+
---
+++ views/store.py
@@ -0,0 +1,74 @@
+from lib2to3.refactor import MultiprocessRefactoringTool
+import discord
+
+from views.basic import ConfirmButton
+
+class MainStoreView(discord.ui.View):
+    """Main menu of the store."""
+
+    
+    
+    def __init__(self, interaction: discord.Interaction, **kwargs):
+        super().__init__(timeout=None)
+        self.interaction = interaction
+        self.main_embed = kwargs.get("main")
+        self.emotes_embed = kwargs.get("emotes")
+        self.roles_embed = kwargs.get("roles")
+        self.emote_dropdown = StoreDropdown(["Standard Emoji", "Animated Emoji", "Sticker"])
+        
+        self.roles = StoreButton("Roles", self.roles_embed)
+        self.emotes = StoreButton("Emotes", self.emotes_embed)
+        self.back = StoreBack()
+        self.confirm = ConfirmButton()
+        self.main_menu_buttons = [self.roles, self.emotes]
+        for item in self.main_menu_buttons:
+            self.add_item(item)
+  
+
+    async def start(self):
+        await self.interaction.edit_original_message(embed=self.main_embed, view=self)
+
+
+
+class StoreButton(discord.ui.Button):
+    def __init__(self, label, embed, row=0):
+        self.view: MainStoreView
+        self.embed = embed
+        super().__init__(label=label, style=discord.ButtonStyle.blurple, row=row)
+
+    async def callback(self, interaction: discord.Interaction):
+        for item in self.view.main_menu_buttons:
+            self.view.remove_item(item)
+        self.view.add_item(self.view.confirm)
+        self.view.add_item(self.view.back)
+        self.view.add_item(self.view.emote_dropdown)     
+        await self.view.interaction.edit_original_message(
+            embed=self.embed, view=self.view
+        )
+
+class StoreBack(discord.ui.Button):
+    def __init__(self, row=0):
+        self.view: MainStoreView
+        super().__init__(label="Back", style=discord.ButtonStyle.grey, row=row)
+
+    async def callback(self, interaction: discord.Interaction):
+        for item in self.view.main_menu_buttons:
+            self.view.add_item(item)
+        self.view.remove_item(self)        
+        await self.view.interaction.edit_original_message(
+            embed=self.view.main_embed, view=self.view
+        )
+
+class StoreDropdown(discord.ui.Select):
+    def __init__(self, values):
+        super().__init__()
+        
+        for value in values:
+            self.add_option(
+                label=value,
+                value=value,
+            )
+
+    async def callback(self, interaction: discord.Interaction):
+        return await super().callback(interaction)
+
