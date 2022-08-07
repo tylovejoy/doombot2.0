5dece29c143641286d90c7375cce2d046c93fadc
Ty Lovejoy <Ty@fromskytocenter.com>
2022-08-07 11:11:07 -0500

    remove sync on startup

--- main.py
+++ main.py
@@ -51,10 +51,7 @@ def load_all_extensions():
 @bot.event
 async def setup():
     """Upload slash commands to discord."""
-
     await database_init()
-    await bot.upload_guild_application_commands()
-    await bot.upload_global_application_commands()
 
 
 TOKEN = environ["TOKEN"]
